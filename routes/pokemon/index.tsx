095fa9aecc5545bca08cd60311406e1082837bae
Forrest S <thewanderinwolves@gmail.com>
2023-02-18 16:02:56 -0800

    added fetch to pokemon index.
    added fetch handler to get all pokemon list.

--- routes/pokemon/index.tsx
+++ routes/pokemon/index.tsx
@@ -1,6 +1,29 @@
 import { Container } from "../../components/Layout.tsx";
-// deno-lint-ignore no-explicit-any
-export default function Home(props: any) {
+import { Handlers, PageProps  } from "$fresh/server.ts";
+
+interface Pokemon {
+    count: number;
+    next: string;
+    previous: string;
+    results: Array<{
+        name: string;
+        url: string;
+    }>;
+}
+
+export const handler: Handlers<Pokemon | null> = {
+    async GET(_, ctx) {
+        // const { username } = ctx.params
+        const resp = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+        if (resp.status === 404) {
+            return ctx.render(null)
+        }
+        const pokemon: Pokemon = await resp.json()
+        return ctx.render(pokemon)
+    }
+}
+
+export default function Page({ data }: PageProps<Pokemon | null>) {
   const meta = {
     title: "Pokemon Fresh",
     description: "I am a description, and I can create multiple tags",
@@ -12,12 +35,14 @@ export default function Home(props: any) {
       },
     },
   };
+  const pokemon = data?.results.map(each => <h2>{each.name}</h2>)
+  
   return (
     <>
       <Container {...meta}>
        
-         <h1>This is the start of the pokemon page</h1>
-      
+        <h1>This is the start of the pokemon page</h1>
+        {pokemon}
       </Container>
     </>
   );
