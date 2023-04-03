2bbc25a4616183e2736c3eb658aec14dff83dcb8
Forrest S <thewanderinwolves@gmail.com>
2023-04-02 18:10:33 -0700

    homepage styling updated.

--- routes/index.tsx
+++ routes/index.tsx
@@ -12,13 +12,16 @@ export default function Home(props: any) {
       },
     },
   };
+
+  const cardStyle = `rounded border-2 border-[#CC0000] p-1 lg:w-1/2 w-5/6`
+  
   return (
       <Layout {...meta}>
         <article class='flex flex-col gap-2 items-center w-1/2 pt-10'>
-          <h1>Welcome to the pokemon viewing website</h1>
-          <p>Checkout out the pokemon link to view all pokemon.</p>
-          <p>Pokemon list and information gathered from <a class='hover:underline' href='https://pokeapi.co/' target='_blank' alt='poke api link'>PokeApi</a></p>
-          <p>The pokedex html/css originated from this <a class='hover:underline' href='https://codepen.io/oryamne/pen/vYKXbgZ?editors=1100' target='_blank' alt='codepen link'>codepen</a>, and then modified to fit different buttons and display information from the poke.api.</p>
+          <h1 class={cardStyle}>Welcome to the pokemon viewing website</h1>
+          <p class={cardStyle}>Checkout out the pokemon link to view all pokemon.</p>
+          <p class={cardStyle}>Pokemon list and information gathered from <a class='hover:underline' href='https://pokeapi.co/' target='_blank' alt='poke api link'>PokeApi</a></p>
+          <p class={cardStyle}>The pokedex html/css originated from this <a class='hover:underline' href='https://codepen.io/oryamne/pen/vYKXbgZ?editors=1100' target='_blank' alt='codepen link'>codepen</a>, and then modified to fit different buttons and display information from the poke.api.</p>
         </article>
       </Layout>
   );
